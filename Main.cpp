4bb1f26488444b3562102baab954e56c7fdf4b0c
crthdr <crthdr00@gmail.com>
2026-08-07 17:45:43 +0300

    rm unused

--- Main.cpp
+++ Main.cpp
@@ -24,15 +24,6 @@ bool NGE = false;
 string NOT_SUPPORTED = string("Game version not supported.");
 
 
-template <typename T>
-std::vector<T> operator+(const std::vector<T>& a, const std::vector<T>& b) {
-    std::vector<T> result;
-    result.reserve(a.size() + b.size());
-    result.insert(result.end(), a.begin(), a.end());
-    result.insert(result.end(), b.begin(), b.end());
-    return result;
-}
-
 string getLastErrorAsString() {
     DWORD errorMessageID = GetLastError();
     if (errorMessageID == 0) return "";
